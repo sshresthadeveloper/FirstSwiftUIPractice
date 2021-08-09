b0308f088bb3518b010a05a20f0a939e5f64ba7d
Suba Shrestha <anything963@hotmail.com>
2021-08-08 22:19:16 -0500

    Initial Commit

---
+++ HackingWithSwift/HackingWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftApp.swift
+//  HackingWithSwift
+//
+//  Created by Shubha Shrestha on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
